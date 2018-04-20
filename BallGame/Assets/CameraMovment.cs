6773b3e46e5bec421091330b08f89056362be371
d15125709 <d15125709@users.noreply.github.com>
2018-04-20 19:27:26 +0100

    camera movment

---
+++ BallGame/Assets/CameraMovment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour {
+
+    public Transform playerBall;
+    public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerBall.position + offset;
+
+	}
+}
