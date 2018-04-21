7c5d6cde3cb38da99586cec8d9b4dde8925b4d83
d15125895 <33666054+d15125895@users.noreply.github.com>
2018-04-21 19:46:27 +0100

    adding a script
    to make the ball stop when it hit somthing

---
+++ BallGame/Assets/PlayerBallCOLLISION.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBallCOLLISION : MonoBehaviour {
+
+    public PlayerBallCOLLISION Movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //  Debug.Log(collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Obsetcle")
+        {
+            Movement.enabled = false;
+           
+
+            // Debug.Log("we hit an Obsetcle");
+        }
+    }
+
+}
+
