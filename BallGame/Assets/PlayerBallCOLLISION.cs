7e7510874979c4f53b0daaa6610283a4d9f657fd
d15125895 <33666054+d15125895@users.noreply.github.com>
2018-04-21 19:58:16 +0100

    display the name of the things that hits

--- BallGame/Assets/PlayerBallCOLLISION.cs
+++ BallGame/Assets/PlayerBallCOLLISION.cs
@@ -1,22 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class PlayerBallCOLLISION : MonoBehaviour {
 
-    public PlayerBallCOLLISION Movement;
+   // public CameraMovment Movement;
 
     void OnCollisionEnter(Collision collisionInfo)
     {
-        //  Debug.Log(collisionInfo.collider.name);
-        if (collisionInfo.collider.tag == "Obsetcle")
-        {
-            Movement.enabled = false;
+         Debug.Log(collisionInfo.collider.name);
+       // if (collisionInfo.collider.tag == "Obsetcle")
+        //{
+          //  Movement.enabled = false;
            
 
-            // Debug.Log("we hit an Obsetcle");
+          //  Debug.Log("we hit an Obsetcle");
         }
     }
 
-}
+
 
