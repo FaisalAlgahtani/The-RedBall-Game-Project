5a64daed62a5616fc8fb953803e50c79b4f444f0
d15125895 <33666054+d15125895@users.noreply.github.com>
2018-04-22 15:32:44 +0100

    adding Game Manager Script

---
+++ BallGame/Assets/GameManagerScript.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public void EndGame()
+    {
+        Debug.Log("Game Over!");
+
+    }
+}
--- BallGame/Assets/Scripts/PlayerBallCOLLISION.cs
+++ BallGame/Assets/Scripts/PlayerBallCOLLISION.cs
@@ -4,14 +4,14 @@ public class PlayerBallCOLLISION : MonoBehaviour
 {
 
    public BallMovement movement;
-
+     
     void OnCollisionEnter (Collision collisionInfo)
     {
        if (collisionInfo.collider.tag == "Obstacle")
        {
 
             movement.enabled = false;
-           
+            FindObjectOfType<GameManagerScript>().EndGame();
         }
     }
 }
