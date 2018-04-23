88205a823bc3821946a19424bd03e0bb7ff57521
d15125709 <d15125709@users.noreply.github.com>
2018-04-23 14:13:57 +0100

    writing Script for animation

--- BallGame/Assets/levelComplete.cs
+++ BallGame/Assets/levelComplete.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class levelComplete : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public void LoadNextLevel ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
